6efdd0777006a5a4614e0d8ebf9fa34ad35c3769
Logan Watkins <lw2014wa@gmail.com>
2024-04-05 07:47:09 -0500

    Add create user logic, DB connections, Schema models

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module blog_aggregator
+module github.com/lowat/blog_aggregator
 
 go 1.22.1
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"net/http"
 	"os"
 
+	"database/sql"
+
+	"github.com/lowat/blog_aggregator/internal/database"
+
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -15,9 +20,25 @@ func main() {
 	}
 	const filepathRoot = "."
 	port := os.Getenv("PORT")
+	dbURL := os.Getenv("CONN")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening db connection")
+	}
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
 	mux := http.NewServeMux()
-	registerPaths(mux)
+	mux.HandleFunc("GET /v1/readiness", handlerReadiness)
+	mux.HandleFunc("GET /v1/err", handlerErr)
+	mux.HandleFunc("POST /v1/users", apiCfg.handlerUsersCreate)
+
 	server := NewServer(port, mux)
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+type apiConfig struct {
+	DB *database.Queries
 }
